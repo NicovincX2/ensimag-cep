15c345bb813cca5db385a8e3d68097ee4cafef4c
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:38:14 +0100

    zero = x0

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -3,10 +3,10 @@
     
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
-    addi zero, zero, 0    # no operation
+    addi x0, x0, 0    # no operation
     addi x31, x0, 0       # copy register
 	addi x31, x0, -2048   # li x31, -2048
-	addi x31, zero, -1    # li x31, 0xFFFFF
+	addi x31, x0, -1    # li x31, 0xFFFFF
 
 	# max_cycle 50
     # 00000000
