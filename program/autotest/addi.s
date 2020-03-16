abfd5082b88cbecc971d3153f2a85e9075b56c18
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:21:05 +0100

    Modification des registres

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -2,9 +2,10 @@
     .text
     
     # addi rd, rs1, imm
-    addi x1, x1, 0       # x1 = 0
-	addi x1, x1, 0xfffff # x1 = valeur maximal sur 20 bits
-	addi x1, x1, 0x12345 # x1 = valeur quelconque
+    # le registre x0 vaut toujours 0
+    addi x1, x0, 0       # x1 = 0
+	addi x1, x0, 0xfffff # x1 = valeur maximal sur 20 bits
+	addi x1, x0, 0x12345 # x1 = valeur quelconque
 
 	# max_cycle 50
     # 00000000
