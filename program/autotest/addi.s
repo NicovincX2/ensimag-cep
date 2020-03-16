86ec2804cdf1c87bf9f2c5f750b8bed84d41d5bf
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:30:05 +0100

    Only x31

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -3,9 +3,9 @@
     
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
-    addi x31, x0, 0       # x1 = 0
-	addi x31, x0, 0xfffff # x1 = valeur maximal sur 20 bits
-	addi x31, x0, 0x12345 # x1 = valeur quelconque
+    addi x31, x31, 0       # x31 = 0
+	addi x31, x31, 0xfffff # x31 = valeur maximal sur 20 bits
+	addi x31, x31, 0x12345 # x31 = valeur quelconque
 
 	# max_cycle 50
     # 00000000
