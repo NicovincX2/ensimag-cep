4cb4d5fec6f1db57e29eef6e41681725adc60aef
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:32:06 +0100

    Decimal

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -4,8 +4,8 @@
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
     addi x31, x31, 0       # x31 = 0
-	addi x31, x31, 0xfffff # x31 = valeur maximal sur 20 bits
-	addi x31, x31, 0x12345 # x31 = valeur quelconque
+	addi x31, x31, 1048575 # x31 = valeur maximal sur 20 bits
+	addi x31, x31, -56 # x31 = valeur quelconque
 
 	# max_cycle 50
     # 00000000
