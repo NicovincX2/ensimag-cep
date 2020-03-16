80a4aae11f486df605749a8c603d09f51b5fdf6f
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:24:53 +0100

    Les valeurs sont comparées auxvaleurs en sortie de pout à chaque écriture du registre 31.

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -3,9 +3,9 @@
     
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
-    addi x1, x0, 0       # x1 = 0
-	addi x1, x0, 0xfffff # x1 = valeur maximal sur 20 bits
-	addi x1, x0, 0x12345 # x1 = valeur quelconque
+    addi x31, x0, 0       # x1 = 0
+	addi x31, x0, 0xfffff # x1 = valeur maximal sur 20 bits
+	addi x31, x0, 0x12345 # x1 = valeur quelconque
 
 	# max_cycle 50
     # 00000000
