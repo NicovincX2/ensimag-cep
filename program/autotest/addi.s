8184177e1ff92b3b11d157b24c7f0a3c370c8401
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 22:46:19 +0100

    change pout

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -3,13 +3,14 @@
     
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
-    addi x0, x0, 0    # no operation
+    addi zero, zero, 0    # no operation
     addi x31, x0, 0       # copy register
 	addi x31, x0, -2048   # li x31, -2048
-	addi x31, x0, -1    # li x31, 0xFFFFF
+	addi x31, zero, -1    # li x31, 0xFFFF
 
 	# max_cycle 50
     # 00000000
-	# FFFFF000
-	# 12345000 
+    # 00000000
+    # F8000000
+	# FFFF0000
 	# pout_end
