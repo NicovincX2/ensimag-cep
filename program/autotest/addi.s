0aad9cda7ce8fbcad25162470d635898d14ec8a9
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2020-03-18 17:35:33 +0100

    Update addi.s

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -8,7 +8,7 @@
 	addi x31, x0, -2048   # li x31, -2048
 	addi x31, zero, -1    # li x31, 0xFFFF
 	addi x31, zero, 2047    # li x31, 0x7F
-	addi x31, zero, 2048    # li x31, 0x800
+	# addi x31, zero, 2048    # li x31, 0x800 fail car range -2048 à 2047
 
 	# max_cycle 50
 	# pout_start
@@ -17,5 +17,4 @@
     # F8000000
 	# FFFF0000
 	# FFFFFFF0
-	# 80000000
 	# pout_end
