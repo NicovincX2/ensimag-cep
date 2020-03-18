a7b9552dd648144fca6049772727a718f7915939
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2020-03-18 17:31:26 +0100

    Update addi.s

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -7,6 +7,7 @@
     addi x31, x0, 0       # copy register
 	addi x31, x0, -2048   # li x31, -2048
 	addi x31, zero, -1    # li x31, 0xFFFF
+	addi x31, zero, 2047    # li x31, 0x7F
 
 	# max_cycle 50
 	# pout_start
@@ -14,4 +15,5 @@
     # 00000000
     # F8000000
 	# FFFF0000
+	# FFFFFFF0
 	# pout_end
