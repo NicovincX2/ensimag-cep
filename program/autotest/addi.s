d9025365d20473ed1ec88d12a2545d93d9a67a02
n0way <n0way@localhost.localdomain>
2020-03-23 10:54:01 +0100

    Correction des valeurs hexadécimales

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -5,15 +5,16 @@
     # le registre x0 vaut toujours 0
     # addi zero, zero, 0    # no operation # 00000000
     addi x31, x0, 0     # copy register
-    # addi x31, zero, -1    # li x31, 0xFFFF
+    addi x31, zero, -1    # li x31, 0xFFFF
 	addi x31, x0, -2048   # li x31, -2048
-	# addi x31, zero, 2047    # li x31, 0x7F
+	addi x31, zero, 2047    # li x31, 0x7F
 	# addi x31, zero, 2048    # li x31, 0x800 fail car range -2048 à 2047
 
 	# max_cycle 50
 	# pout_start
     # 00000000
+    # FFFFFFFF
     # FFFFF800
+    # 000007FF
 	# pout_end
-	# FFFF0000
 	# FFFFFFF0
