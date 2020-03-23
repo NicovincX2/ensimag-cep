320686a3d8443a7bbaa7a80b7d3765f8734dc19b
n0way <n0way@localhost.localdomain>
2020-03-23 10:22:51 +0100

    Test addi

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -4,10 +4,10 @@
     # addi rd, rs1, imm
     # le registre x0 vaut toujours 0
     # addi zero, zero, 0    # no operation # 00000000
-    addi x31, x0, 0       # copy register
-	addi x31, x0, -2048   # li x31, -2048
-	addi x31, zero, -1    # li x31, 0xFFFF
-	addi x31, zero, 2047    # li x31, 0x7F
+    addi x31, x0, 0     # copy register
+	# addi x31, x0, -2048   # li x31, -2048
+	# addi x31, zero, -1    # li x31, 0xFFFF
+	# addi x31, zero, 2047    # li x31, 0x7F
 	# addi x31, zero, 2048    # li x31, 0x800 fail car range -2048 à 2047
 
 	# max_cycle 50
