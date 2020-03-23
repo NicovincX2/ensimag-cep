8920a020211d4798ec23d76d94fc1c27801ffb0e
n0way <n0way@localhost.localdomain>
2020-03-23 11:27:11 +0100

    test add

--- program/autotest/add.s
+++ program/autotest/add.s
@@ -9,23 +9,26 @@
     addi x2, x0, 1
     add x31, x1, x2
 
-    # addi x1, x0, 5
-    # addi x2, x1, 6
-    # add x31, x1, x2
+    add x31, x0, x0
+    addi x1, x0, 5
+    addi x2, x0, -5
+    add x31, x1, x2
 
-    # addi x1, x0, -2048
-    # addi x2, x1, 0
-    # add x31, x1, x2
-    #
-    # addi x1, x0, 2046
-    # addi x2, x1, 1
-    # add x31, x1, x2
+    add x31, x0, x0
+    addi x1, x0, -2048
+    addi x2, x0, 0
+    add x31, x1, x2
+
+    add x31, x0, x0
+    addi x1, x0, 2046
+    addi x2, x0, 1
+    add x31, x1, x2
 
 	# max_cycle 50
 	# pout_start
     # 00000000
     # 00000002
-	# pout_end
-    # 0000000B
+    # 00000000
     # FFFFF800
     # 000007FF
+	# pout_end
