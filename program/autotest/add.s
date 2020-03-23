d84aa32f51461a2444603a2761a34cb2d182ece0
n0way <n0way@localhost.localdomain>
2020-03-23 11:08:02 +0100

    Test add

--- program/autotest/add.s
+++ program/autotest/add.s
@@ -1,7 +1,7 @@
 # TAG = add
     .text
 
-    # addi rd, rs1, imm
+    # add rd, rs1, rs2
     # le registre x0 vaut toujours 0
     add x31, zero, zero    # no operation
 
@@ -12,19 +12,19 @@
     addi x2, x1, 6
     add x31, x1, x2
 
-    addi x1, x0, -2048
-    addi x2, x1, 0
-    add x31, x1, x2
-
-    addi x1, x0, 2046
-    addi x2, x1, 1
-    add x31, x1, x2
+    # addi x1, x0, -2048
+    # addi x2, x1, 0
+    # add x31, x1, x2
+    #
+    # addi x1, x0, 2046
+    # addi x2, x1, 1
+    # add x31, x1, x2
 
 	# max_cycle 50
 	# pout_start
     # 00000000
     # 00000001
+	# pout_end
     # 0000000B
     # FFFFF800
     # 000007FF
-	# pout_end
