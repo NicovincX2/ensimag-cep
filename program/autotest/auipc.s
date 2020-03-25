fa51051a5f0fa81764ae93cf7a51d737d8a396bc
n0way <n0way@localhost.localdomain>
2020-03-25 11:58:02 +0100

    abandon

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -3,7 +3,7 @@
 
 	auipc x31, 0       #Test chargement d'une valeur nulle PC = 0x1000
     auipc x31, 0       #Test chargement de PC = 0x1004
-	auipc x31, 5 #Test chargement d'une valeur maximal sur 20 bits PC = 0x1008
+	auipc x31, 5 #Test chargement d'une valeur maximal sur 20 bits PC = 0x1008 FAILED
 
 	# max_cycle 50
 	# pout_start
