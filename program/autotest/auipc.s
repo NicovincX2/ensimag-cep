3a248c200539dfc2b7dd0c3fbb1a6ca565ba35f9
n0way <n0way@localhost.localdomain>
2020-03-25 11:05:04 +0100

    Test auipc

---
+++ program/autotest/auipc.s
@@ -0,0 +1,13 @@
+# TAG = auipc
+	.text
+
+	auipc x31, 0       #Test chargement d'une valeur nulle PC = 0x1000
+	auipc x31, 0xfffff #Test chargement d'une valeur maximal sur 20 bits PC = 0x1388
+	#auipc x31, 0x12345 #Test chargement d'une valeur quelconque PC = 0x138c
+
+	# max_cycle 50
+	# pout_start
+	# 00001000
+	# 00001388
+	# 12345000
+	# pout_end
