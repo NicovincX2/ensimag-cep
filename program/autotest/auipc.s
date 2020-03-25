4719a6f6d1383ee25a9bfad2e9010da161d8765d
n0way <n0way@localhost.localdomain>
2020-03-25 11:55:23 +0100

    c ptet le bon...

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -3,11 +3,11 @@
 
 	auipc x31, 0       #Test chargement d'une valeur nulle PC = 0x1000
     auipc x31, 0       #Test chargement de PC = 0x1004
-	auipc x31, 0 #Test chargement d'une valeur maximal sur 20 bits PC = 0x1008
+	auipc x31, 5 #Test chargement d'une valeur maximal sur 20 bits PC = 0x1008
 
 	# max_cycle 50
 	# pout_start
 	# 00001000
     # 00001004
-    # 00001008
+    # 0000000c
 	# pout_end
