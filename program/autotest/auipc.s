3f6998f4d83705d37a11e952571d4660cd7ddd31
n0way <n0way@localhost.localdomain>
2020-03-30 08:45:22 +0200

    auipc

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -5,7 +5,7 @@
 
 	auipc x31, 0          # Test chargement de PC = 0x1000
     auipc x31, 0          # Test chargement de PC = 0x1004
-	auipc x31, 0x000feff7 # 0x000feff7 + 0x1008 = 0x000fffff
+	auipc x31, 0xfeff7 # 0x000feff7 + 0x1008 = 0x000fffff
 	#auipc x31, 0x000ef459 # 0x000ef459 + 0x100c = 0x000F0465
 
 	# max_cycle 50
