65335b9091d68d3d7038b5baf78a571dcfd24c98
n0way <n0way@localhost.localdomain>
2020-03-30 08:19:56 +0200

    Test auipc

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -1,13 +1,15 @@
 # TAG = auipc
 	.text
 
-	auipc x31, 0       #Test chargement d'une valeur nulle PC = 0x1000
-    auipc x31, 0       #Test chargement de PC = 0x1004
-	#auipc x31, 5 #Test chargement d'une valeur maximal sur 20 bits PC = 0x1008 FAILED
+	auipc x31, 0          # Test chargement de PC = 0x1000
+    auipc x31, 0          # Test chargement de PC = 0x1004
+	auipc x31, 0xabcdef00 # 0xabcdef00 + 0x1008 = 0xabcdff08
+	auipc x31, 0xef459000 # 0xef459000 + 0x100c = 0xef45a00c
 
 	# max_cycle 50
 	# pout_start
 	# 00001000
     # 00001004
+	# abcdff08
+	# ef45a00c
 	# pout_end
-    # 00000009
