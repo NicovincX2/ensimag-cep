c5cec41fd0376f5b7fbb58e6f6ebd286fd2c1f51
n0way <n0way@localhost.localdomain>
2020-03-30 10:50:49 +0200

    cnd

--- vhd/CPU_CND.vhd
+++ vhd/CPU_CND.vhd
@@ -21,9 +21,16 @@ architecture RTL of CPU_CND is
     signal z : std_logic;
     signal s : std_logic;
 begin
-    -- extension_signe <= (not(IR(12)) and not(IR(6))) or (IR(6) and not(IR(13)));
-    -- z <= ((rs1 - alu_y) + extension_signe) or not 0;
-    -- s <= ((rs1 - alu_y) + extension_signe)(31);
-    -- jcond <= (not(IR(14)) and (IR(12) xor z)) or ((s xor IR(12)) and IR(14));
-    -- slt <= s;
+
+    -- To extend an unsigned value, just stick a zero on the front: ('0' & a)
+    -- To extend a signed value, you need to copy the existing sign bit: (b(N-1) & b)
+    -- (where N is the length of B, so N = 8 in this case).
+    -- The resulting code would be: result <= ('0' & a) + (b(7) & b);
+        
+    extension_signe <= (not(IR(12)) and not(IR(6))) or (IR(6) and not(IR(13)));
+    z <= ((rs1 - alu_y) & extension_signe) or not 0;
+    s <= ((rs1 - alu_y) & extension_signe)(31);
+    jcond <= (not(IR(14)) and (IR(12) xor z)) or ((s xor IR(12)) and IR(14));
+    slt <= s;
+
 end architecture;
