0f071c56a55f9382eeec68eab32974ca418dcdc7
n0way <n0way@localhost.localdomain>
2020-03-30 09:17:40 +0200

    auipc

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -2,16 +2,17 @@
 	.text
 
 	# auipc.s:6: Error: lui expression not in range 0..1048575 (0..0x000fffff)
+	# upper : ne touche pas aux 3 derniers octets
 
 	auipc x31, 0          # Test chargement de PC = 0x1000
     auipc x31, 0          # Test chargement de PC = 0x1004
-	auipc x31, 0xfeff7 # 0x000feff7 + 0x1008 = 0x000fffff
-	#auipc x31, 0x000ef459 # 0x000ef459 + 0x100c = 0x000F0465
+	#auipc x31, 0xfeff7 # 0x000feff7 + 0x1008 = 0x000fffff
+	auipc x31, 0xef459000 # 0x000ef459 + 0x1008 = 0x000f0461
 
 	# max_cycle 50
 	# pout_start
 	# 00001000
     # 00001004
-	# 000fffff
+	# 000f0461
 	# pout_end
-	# 000f0465
+	# 000fffff
