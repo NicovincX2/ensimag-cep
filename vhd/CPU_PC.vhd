6e9f1cdf653514a8ffc27bfb7d0e39960eeb3e2d
n0way <n0way@localhost.localdomain>
2020-04-01 09:58:51 +0200

    en if;

--- vhd/CPU_PC.vhd
+++ vhd/CPU_PC.vhd
@@ -522,6 +522,7 @@ begin
                     cmd.TO_PC_Y_sel <= TO_PC_Y_cst_x04;
                     cmd.PC_sel <= PC_from_pc;
                     cmd.PC_we <= '1';
+                end if;
                 -- lecture mem[PC], fait dans Pre_Fetch
                 -- cmd.ADDR_sel <= ADDR_from_pc;
                 -- cmd.mem_ce <= '1';
