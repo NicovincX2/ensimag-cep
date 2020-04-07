7a46cce5cd95ac1e7f82eb6f402253d501f9dd99
n0way <n0way@localhost.localdomain>
2020-04-07 16:22:08 +0200

    added

--- program/autotest/and.s
+++ program/autotest/and.s
@@ -18,6 +18,19 @@
     li x2, 0xf0f0f0f0
     and x31, x1, x2
 
+    # Groupement 2
+    li x1, 0xff00ff00
+    and x31, x1, zero
+
+    li x1, 0x00ff00ff
+    and x31, zero, x1
+
+    and x31, zero, zero
+
+    li x1, 0x11111111
+    li x2, 0x22222222
+    and x0, x1, x2  # limitation de x31
+
     # TODO : 
     # source/destination tests
     # tester pour differents nombre de cycles (ce ne sont pas des tests unitaires)
@@ -28,4 +41,7 @@
     # 00f000f0
     # 000f000f
     # f000f000
+    # 00000000
+    # 00000000
+    # 00000000
     # pout_end
