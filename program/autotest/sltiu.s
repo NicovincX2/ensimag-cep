798c7d78e6614a0f228cd7a66a28e9356a447b5e
n0way <n0way@localhost.localdomain>
2020-04-07 20:02:09 +0200

    cor

--- program/autotest/sltiu.s
+++ program/autotest/sltiu.s
@@ -17,46 +17,46 @@
 
     # Groupement 2
     li x1, 0x00000000
-    sltiu x31, x1, 0xfffff800
+    sltiu x31, x1, 0x00000800
 
     li x1, 0x80000000
     sltiu x31, x1, 0x000
 
     li x1, 0x80000000
-    sltiu x31, x1, 0xfffff800
+    sltiu x31, x1, 0x00000800
 
     # Groupement 3
     li x1, 0x00000000
-    sltiu x31, x1, 0xfffff7ff
+    sltiu x31, x1, 0x7ff
 
     li x1, 0x7fffffff
     sltiu x31, x1, 0x000
 
     li x1, 0x7fffffff
-    sltiu x31, x1, 0xfffff7ff
+    sltiu x31, x1, 0x7ff
 
     # Groupement 4
     li x1, 0x80000000
-    sltiu x31, x1, 0xfffff7ff
+    sltiu x31, x1, 0x7ff
 
     li x1, 0x7fffffff
-    sltiu x31, x1, 0xfffff800
+    sltiu x31, x1, 0x00000800
 
     # Groupement 5
     li x1, 0x00000000
-    sltiu x31, x1, 0xffffffff
+    sltiu x31, x1, 0x00000fff
 
     li x1, 0xffffffff
     sltiu x31, x1, 0x001
 
     li x1, 0xffffffff
-    sltiu x31, x1, 0xffffffff
+    sltiu x31, x1, 0x00000fff
 
     # Groupement 6
-    sltiu x31, x0, 0xffffffff
+    sltiu x31, x0, 0x00000fff
 
     li x1, 0x00ff00ff
-    sltiu x0, x1, 0xffffffff  # mv x31, x0
+    sltiu x0, x1, 0x00000fff  # mv x31, x0
 
 	# max_cycle 300
 	# pout_start
