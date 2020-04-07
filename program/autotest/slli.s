f313fd2f411c0900b7ffb6c7932435e76f5f09e0
n0way <n0way@localhost.localdomain>
2020-04-07 18:50:20 +0200

    slli

--- program/autotest/slli.s
+++ program/autotest/slli.s
@@ -4,74 +4,57 @@
     # Toujours plus de tests...
     # Groupement 1
     li x1, 0x00000001
-    li x2, 0
-    slli x31, x1, x2
+    slli x31, x1, 0
 
     li x1, 0x00000001
-    li x2, 1
-    slli x31, x1, x2
+    slli x31, x1, 1
 
     li x1, 0x00000001
-    li x2, 7
-    slli x31, x1, x2
+    slli x31, x1, 7
 
     li x1, 0x00000001
-    li x2, 14
-    slli x31, x1, x2
+    slli x31, x1, 14
 
     li x1, 0x00000001
-    li x2, 31
-    slli x31, x1, x2
+    slli x31, x1, 31
 
     # Groupement 2
     li x1, 0xffffffff
-    li x2, 0
-    slli x31, x1, x2
+    slli x31, x1, 0
 
     li x1, 0xffffffff
-    li x2, 1
-    slli x31, x1, x2
+    slli x31, x1, 1
 
     li x1, 0xffffffff
-    li x2, 7
-    slli x31, x1, x2
+    slli x31, x1, 7
 
     li x1, 0xffffffff
-    li x2, 14
-    slli x31, x1, x2
+    slli x31, x1, 14
 
     li x1, 0xffffffff
-    li x2, 31
-    slli x31, x1, x2
+    slli x31, x1, 31
 
     # Groupement 3
     li x1, 0x21212121
-    li x2, 0
-    slli x31, x1, x2
+    slli x31, x1, 0
 
     li x1, 0x21212121
-    li x2, 1
-    slli x31, x1, x2
+    slli x31, x1, 1
 
     li x1, 0x21212121
-    li x2, 7
-    slli x31, x1, x2
+    slli x31, x1, 7
 
     li x1, 0x21212121
-    li x2, 14
-    slli x31, x1, x2
+    slli x31, x1, 14
 
     li x1, 0x21212121
-    li x2, 31
-    slli x31, x1, x2
+    slli x31, x1, 31
 
     # Groupement 4
-    li x1, 15
-    slli x31, x0, x1
+    slli x31, x0, 15
 
     li x1, 1024
-    li x2, 2048
-    slli x0, x1, x2  # mv x31, x0
+    slli x0, x1, 2048  # mv x31, x0
 
 	# max_cycle 300
 	# pout_start
