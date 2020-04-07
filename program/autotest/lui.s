7030d7e05776fd8b22ac92d4537cc667d1e82cd1
n0way <n0way@localhost.localdomain>
2020-04-07 17:47:48 +0200

    sra

--- program/autotest/lui.s
+++ program/autotest/lui.s
@@ -3,15 +3,14 @@
 
 	lui x31, 0       #Test chargement d'une valeur nulle
 	lui x31, 0xfffff #Test chargement d'une valeur maximal sur 20 bits
-	# sra x31, x31, 1
 	lui x31, 0x12345 #Test chargement d'une valeur quelconque
 
 	# D'autres tests
-	lui x31, 0x0007ffff
-	# sra x31, x31, 20
+	lui x1, 0x7ffff
+	sra x31, x1, 20
 
-	lui x31, 0x00080000
-	# sra x31, x31, 20
+	lui x1, 0x80000
+	sra x31, x1, 20
 
 	lui x0, 0x80000  # à mv dans x31
 
