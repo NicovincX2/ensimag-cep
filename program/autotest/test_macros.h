c9687f1e8b1e8c039a373422b02e3dda7feb4792
n0way <n0way@localhost.localdomain>
2020-04-07 14:33:22 +0200

    retest macro

--- program/autotest/test_macros.h
+++ program/autotest/test_macros.h
@@ -1,11 +1,14 @@
 // On essaie de généraliser nos tests
 
+#ifndef __test_macros_h__
+#define __test_macros_h__
 
-#-----------------------------------------------------------------------
-# Tests for an instruction with register-register operands
-#-----------------------------------------------------------------------
 
-#define TEST_RR_OP(instruction, val1, val2 ) \
+// Tests for an instruction with register-register operands
+
+#define TEST_RR_OP(instruction, val1, val2) \
     li  x1, val1; \
     li  x2, val2; \
-    instruction x3, x1, x2;
+    instruction x31, x1, x2;
+
+#endif
