d5e80b73bcf27c44b9b7c68c5ce67b9f0ee5228c
n0way <n0way@localhost.localdomain>
2020-04-07 15:28:57 +0200

    tests arithmétiques

--- program/autotest/add.s
+++ program/autotest/add.s
@@ -1,6 +1,6 @@
 # TAG = add
 
-    # le registre x0 vaut toujours 0
+    # Tests normaux
     add x31, zero, zero  # no operation
     addi x1, x0, 1
     addi x2, x0, 1
@@ -11,6 +11,7 @@
     addi x2, x0, -5
     add x31, x1, x2
 
+    # Quelques cas limites
     add x31, x0, x0
     addi x1, x0, -2048
     addi x2, x0, 0
@@ -21,7 +22,8 @@
     addi x2, x0, 1
     add x31, x1, x2
 
-    # On passe en hexa avec lui
+    # On passe en hexa avec lui et d'autres cas limites
+    # Groupement 1
     li x1, 0xffff8000 
     li x2, 0x00000000
     add x31, x1, x2
@@ -30,8 +32,52 @@
     li x2, 0x80000000
     add x31, x1, x2
 
+    li x1, 0x7fff8000
+    li x2, 0x80000000
+    add x31, x1, x2
 
-	# max_cycle 100
+    # Groupement 2
+    li x1, 0x00007fff 
+    li x2, 0x00000000
+    add x31, x1, x2
+
+    li x1, 0x7fffffff
+    li x2, 0x7fffffff
+    add x31, x1, x2
+
+    li x1, 0x80007ffe
+    li x2, 0x7fffffff
+    add x31, x1, x2
+
+    # Groupement 3
+    li x1, 0x80007fff 
+    li x2, 0x80000000
+    add x31, x1, x2
+
+    li x1, 0x7fff7fff
+    li x2, 0x7fffffff
+    add x31, x1, x2
+
+    # Groupement 4
+    li x1, 0xffffffff 
+    li x2, 0x00000000
+    add x31, x1, x2
+
+    li x1, 0x00000000
+    li x2, 0xffffffff
+    add x31, x1, x2
+
+    li x1, 0xfffffffe
+    li x2, 0xffffffff
+    add x31, x1, x2
+
+    # Groupement 5
+    li x1, 0x80000000 
+    li x2, 0x00000001
+    add x31, x1, x2
+
+
+	# max_cycle 300
 	# pout_start
     # 00000000
     # 00000002
@@ -43,4 +89,14 @@
     # 000007FF
     # ffff8000
     # 00000000
+    # ffff8000
+    # 00007fff
+    # 00000000
+    # 00007fff
+    # 00007fff
+    # ffff8000
+    # ffffffff
+    # 00000001
+    # ffffffff
+    # 7fffffff
 	# pout_end
