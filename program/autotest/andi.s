c5d98f08b0f3d9c751e8755c97b7657514674424
n0way <n0way@localhost.localdomain>
2020-04-07 16:29:34 +0200

    andi

---
+++ program/autotest/andi.s
@@ -0,0 +1,34 @@
+# TAG = andi
+    .text
+
+    # Groupement 1
+    li x1, 0xff00ff00
+    andi x31, x1, 0xf0f
+
+    li x1, 0x0ff00ff0
+    andi x31, x1, 0x0f0
+
+    li x1, 0x00ff00ff
+    andi x31, x1, 0x70f
+
+    li x1, 0xf00ff00f
+    andi x31, x1, 0x0f0
+
+    # Groupement 2
+    andi x31, zero, 0x0f0
+
+    li x1, 0x00ff00ff
+    andi x0, x1, 0x70f  # limitation de x31
+
+    # TODO : 
+    # source/destination tests
+    # tester pour differents nombre de cycles (ce ne sont pas des tests unitaires)
+
+	# max_cycle 100
+	# pout_start
+    # ff00ff00
+    # 000000f0
+    # 0000000f
+    # 00000000
+    # 00000000
+    # pout_end
