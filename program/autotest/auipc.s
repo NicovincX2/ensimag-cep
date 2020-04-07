dcd3c57a6258ee9a21244003eaaa43e55c98b91f
n0way <n0way@localhost.localdomain>
2020-04-07 16:38:55 +0200

    auipc test

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -10,6 +10,11 @@
 	auipc x31, 0xef459    # 0xef459000 + 0x1008 = 0xef45a008
 	auipc x31, 0xffffe    # 0xffffe000 + 0x100c = 0xfffff00c
 
+	.align 3
+    lla a0, 1f + 10000
+    jal a1, 1f
+    1: sub x31, a0, a1
+
 	# 0xfffff000 + 0x100c = 0x10000000C
 
 	# max_cycle 50
@@ -18,4 +23,5 @@
     # 00001004
 	# ef45a008
 	# fffff00c
+	# 00002710
 	# pout_end
