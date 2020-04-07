855133a2dac91b00d97784e79da7cdde6eebdaec
n0way <n0way@localhost.localdomain>
2020-04-07 21:00:13 +0200

    cor

--- program/autotest/srl.s
+++ program/autotest/srl.s
@@ -27,80 +27,80 @@
     srl x31, x1, x2
 
     # Groupement 2
-    # li x1, 0x7fffffff
-    # li x2, 0
-    # srl x31, x1, x2
+    li x1, 0x7fffffff
+    li x2, 0
+    srl x31, x1, x2
 
-    # li x1, 0x7fffffff
-    # li x2, 1
-    # srl x31, x1, x2
+    li x1, 0x7fffffff
+    li x2, 1
+    srl x31, x1, x2
 
-    # li x1, 0x7fffffff
-    # li x2, 7
-    # srl x31, x1, x2
+    li x1, 0x7fffffff
+    li x2, 7
+    srl x31, x1, x2
 
-    # li x1, 0x7fffffff
-    # li x2, 14
-    # srl x31, x1, x2
+    li x1, 0x7fffffff
+    li x2, 14
+    srl x31, x1, x2
 
-    # li x1, 0x7fffffff
-    # li x2, 31
-    # srl x31, x1, x2
+    li x1, 0x7fffffff
+    li x2, 31
+    srl x31, x1, x2
 
-    # # Groupement 3
-    # li x1, 0x21212121
-    # li x2, 0
-    # srl x31, x1, x2
+    # Groupement 3
+    li x1, 0x21212121
+    li x2, 0
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 1
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 1
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 7
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 7
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 14
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 14
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 31
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 31
+    srl x31, x1, x2
 
-    # # Groupement 4
-    # li x1, 0x21212121
-    # li x2, 0xffffffc0
-    # srl x31, x1, x2
+    # Groupement 4
+    li x1, 0x21212121
+    li x2, 0xffffffc0
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 0xffffffc1
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 0xffffffc1
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 0xffffffc7
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 0xffffffc7
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 0xffffffce
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 0xffffffce
+    srl x31, x1, x2
 
-    # li x1, 0x21212121
-    # li x2, 0xffffffff
-    # srl x31, x1, x2
+    li x1, 0x21212121
+    li x2, 0xffffffff
+    srl x31, x1, x2
 
-    # # Groupement 5
-    # li x1, 15
-    # srl x31, x0, x1
+    # Groupement 5
+    li x1, 15
+    srl x31, x0, x1
 
-    # li x1, 32
-    # srl x31, x1, x0
+    li x1, 32
+    srl x31, x1, x0
 
-    # srl x31, zero, zero
+    srl x31, zero, zero
 
-    # li x1, 1024
-    # li x2, 2048
-    # srl x0, x1, x2  # mv x31, x0
+    li x1, 1024
+    li x2, 2048
+    srl x0, x1, x2  # mv x31, x0
 
 	# max_cycle 300
 	# pout_start
@@ -109,13 +109,11 @@
     # 01000000
     # 00020000
     # 00000001
-	# pout_end
-
-    # ffffffff
     # 7fffffff
-    # 01ffffff
-    # 003fffff
-    # 00000001
+    # 3fffffff
+    # 00ffffff
+    # 0001ffff
+    # 00000000
     # 21212121
     # 10909090
     # 00424242
@@ -129,3 +127,4 @@
     # 00000000
     # 00000020
     # 00000000
+	# pout_end
