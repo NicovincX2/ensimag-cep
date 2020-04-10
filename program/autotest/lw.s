3ed2af025cf116158bc36b23d349c9e3bca06853
n0way <n0way@localhost.localdomain>
2020-04-10 13:08:56 +0200

    tdat4

--- program/autotest/lw.s
+++ program/autotest/lw.s
@@ -2,18 +2,18 @@
 
     .text
 
-    # test avec offset positif
-    la x1, tdat
-    lw x31, 0(x1)
+    # # test avec offset positif
+    # la x1, tdat
+    # lw x31, 0(x1)
 
-    la x1, tdat
-    lw x31, 4(x1)
+    # la x1, tdat
+    # lw x31, 4(x1)
 
-    la x1, tdat
-    lw x31, 8(x1)
+    # la x1, tdat
+    # lw x31, 8(x1)
 
-    la x1, tdat
-    lw x31, 12(x1)
+    # la x1, tdat
+    # lw x31, 12(x1)
 
     # test avec offset négatif
     la x1, tdat4
@@ -28,25 +28,25 @@
     la x1, tdat4
     lw x31, 0(x1)
 
-    # test avec base négative
-    la x1, tdat
-    addi x1, x1, -32
-    lw x31, 32(x1)
+    # # test avec base négative
+    # la x1, tdat
+    # addi x1, x1, -32
+    # lw x31, 32(x1)
 
-    # test avec base non alignée
-    la x1, tdat
-    addi x1, x1, -3
-    lw x31, 7(x1)
+    # # test avec base non alignée
+    # la x1, tdat
+    # addi x1, x1, -3
+    # lw x31, 7(x1)
 
-    # test écriture après lw
-    la x1, tdat
-    lw x31, 0(x1)
-    li x31, 2
+    # # test écriture après lw
+    # la x1, tdat
+    # lw x31, 0(x1)
+    # li x31, 2
 
-    la x1, tdat
-    lw x31, 0(x1)
-    nop
-    li x31, 2
+    # la x1, tdat
+    # lw x31, 0(x1)
+    # nop
+    # li x31, 2
 
     .data
     
@@ -59,10 +59,16 @@
     # ff00ff00
     # 0ff00ff0
     # f00ff00f
+    # pout_end
+
+    # 00000000
+    # 00000000
+    # 00000000
+    # 00000000
+
     # 00ff00ff
     # ff00ff00
     # 00ff00ff
     # 00000002
     # 00ff00ff
     # 00000002
-    # pout_end
