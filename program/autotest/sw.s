de1e7bde344b629fd5fb3f060fc22647ea967981
Adrien <adrien.argento2@grenoble-inp.org>
2020-04-12 21:39:54 +0200

    modifs sw

--- program/autotest/sw.s
+++ program/autotest/sw.s
@@ -7,19 +7,19 @@
 	sw x1, 4(x2)
 	lw x31, 4(x2)
 
-	lui x1, 54594
+	lui x1, -54594
 	sw x1, 8(x2)
 	lw x31, 8(x2)
 
-	lui x1, 4143243
+	lui x1, 132314
 	sw x1, 12(x2)
 	lw x31, 12(x2)
 
 	#max_cycle 300
 	#pout_start
 	#00000000
-	#0000D542
-	#003F388B
+	#FFFF2ABE
+	#000204DA
 	#pout_end
 
 	.data
