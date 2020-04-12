0fd09b91799208d17cdf0143800467c578a93f9b
Adrien <adrien.argento2@grenoble-inp.org>
2020-04-12 18:07:59 +0200

    Modifs lw

--- program/autotest/lw.s
+++ program/autotest/lw.s
@@ -1,81 +1,32 @@
-# TAG = lw
+#TAG = lw
 
-    .text
+	.text
 
-    la x1, test
-    lw x31, 0(x1)
+	#Offset positif
+	
+	la x1, ADDR1
+	lw x31, 0(x1)
 
-    .data
-    sentinelle: .word 0xFFFFFFFF
-    test: .word 0xf00ff00f
-    # tdat: 
-    # tdat4: .word 0xf00ff00f
+	xor x1, x1
+	la x1, ADDR1
+	lw x31, 4(x1)
 
-    # On pourrait faire des load d'instructions mais je ne vois pas l'intérêt.
+	xor x1, x1
+	la x1, ADDR1
+	lw x31, 8(x1)
 
-    # test avec offset positif
-    # la x1, tdat
-    # lw x31, 0(x1)
+	xor x1, x1
+	la x1, ADDR1
+	lw x31, 12(x1)
 
-    # la x1, tdat
-    # lw x31, 4(x1)
+	#max_cycle 300
+	#pout_start
+	#F00FF00F
+	#F00FF013
+	#F00FF017
+	#F00FF01B
+	#pout_end
 
-    # la x1, tdat
-    # lw x31, 8(x1)
+	.data
+	ADDR1: .word 0xF00FF00F
 
-    # la x1, tdat
-    # lw x31, 12(x1)
-
-    # test avec offset négatif
-    # la x1, tdat4
-    # lw x31, -12(x1)
-
-    # la x1, tdat4
-    # lw x31, -8(x1)
-
-    # la x1, tdat4
-    # lw x31, -4(x1)
-
-    # la x1, tdat4
-    # lw x31, 0(x1)
-
-    # # test avec base négative
-    # la x1, tdat
-    # addi x1, x1, -32
-    # lw x31, 32(x1)
-
-    # # test avec base non alignée
-    # la x1, tdat
-    # addi x1, x1, -3
-    # lw x31, 7(x1)
-
-    # # test écriture après lw
-    # la x1, tdat
-    # lw x31, 0(x1)
-    # li x31, 2
-
-    # la x1, tdat
-    # lw x31, 0(x1)
-    # nop
-    # li x31, 2
-
-    # max_cycle 300
-	# pout_start
-    # f00ff00f
-    # pout_end
-
-
-    # 00ff00ff
-    # ff00ff00
-    # 0ff00ff0
-    # f00ff00f
-    # 00ff00ff
-    # ff00ff00
-    # 0ff00ff0
-    # f00ff00f
-    # 00ff00ff
-    # ff00ff00
-    # 00ff00ff
-    # 00000002
-    # 00ff00ff
-    # 00000002
