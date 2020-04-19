84d5b541d89503778f701e863969e1767682f3f6
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2020-04-19 18:15:15 +0200

    Update sw.s

--- program/autotest/sw.s
+++ program/autotest/sw.s
@@ -14,6 +14,66 @@
 	lui x1, 132314
 	sw x1, 12(x2)
 	lw x31, 12(x2)
+	
+	# Les tests qui suivent sont corrects mais ne fonctionnent
+	# pas à cause d'un problème de génération du fichier .mem
+	# Offset positif
+	# la x1, testdata
+	# li x2, 0x00aa00aa
+	# sw x2, 0(x1)
+	# lw x31, 0(x1)
+
+    # la x1, testdata
+	# li x2, 0xaa00aa00
+	# sw x2, 4(x1)
+	# lw x31, 4(x1)
+	
+	# la x1, testdata
+	# li x2, 0x0aa00aa0
+	# sw x2, 8(x1)
+	# lw x31, 8(x1)
+	
+	# la x1, testdata
+	# li x2, 0xa00aa00a
+	# sw x2, 12(x1)
+	# lw x31, 12(x1)
+	
+	# Offset négatif
+	# la x1, testdata8
+	# li x2, 0x00aa00aa
+	# sw x2, -12(x1)
+	# lw x31, -12(x1)
+
+    # la x1, testdata8
+	# li x2, 0xaa00aa00
+	# sw x2, -8(x1)
+	# lw x31, -8(x1)
+	
+	# la x1, testdata8
+	# li x2, 0x0aa00aa0
+	# sw x2, -4(x1)
+	# lw x31, -4(x1)
+	
+	# la x1, testdata8
+	# li x2, 0xa00aa00a
+	# sw x2, 0(x1)
+	# lw x31, 0(x1)
+	
+	# Base négative
+	# la  x1, testdata9
+    # li  x2, 0x12345678
+    # addi x4, x1, -32
+    # sw x2, 32(x4)
+    # lw x31, 0(x1)
+	
+	# Base non alignée
+	# la  x1, testdata9
+    # li  x2, 0x58213098
+    # addi x1, x1, -3
+    # sw x2, 7(x1)
+    # la  x4, testdata10
+    # lw x31, 0(x4)
+	
 
 	# max_cycle 300
 	# pout_start
@@ -21,7 +81,29 @@
 	# 0000D542
 	# 000204DA
 	# pout_end
-
+	
+	# 00aa00aa
+    # aa00aa00
+    # 0aa00aa0
+    # a00aa00a
+    # 00aa00aa
+    # aa00aa00
+    # 0aa00aa0
+    # a00aa00a
+    # 12345678
+    # 58213098
+    
 	.data
 	ADDR1: .word 0xF00FF00F
+	testdata:
+    testdata1:  .word 0xdeadbeef
+    testdata2:  .word 0xdeadbeef
+    testdata3:  .word 0xdeadbeef
+    testdata4:  .word 0xdeadbeef
+    testdata5:  .word 0xdeadbeef
+    testdata6:  .word 0xdeadbeef
+    testdata7:  .word 0xdeadbeef
+    testdata8:  .word 0xdeadbeef
+    testdata9:  .word 0xdeadbeef
+    testdata10: .word 0xdeadbeef
 
