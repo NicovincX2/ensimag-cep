062155d8b8d11773f771d69d9a90f460232730a5
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2020-04-19 18:27:32 +0200

    Add lb.s

---
+++ program/autotest/lb.s
@@ -0,0 +1,77 @@
+#TAG = lb
+
+	.text
+
+	# Les tests qui suivent devraient fonctionner mais
+	# non à cause d'un mauvaise génération des fichiers .mem
+	# test avec offset positif
+    # la x1, testdata
+    # lb x31, 0(x1)
+
+    # la x1, testdata
+    # lb x31, 1(x1)
+
+    # la x1, testdata
+    # lb x31, 2(x1)
+
+    # la x1, testdata
+    # lb x31, 3(x1)
+
+    # test avec offset négatif
+    # la x1, testdata4
+    # lb x31, -3(x1)
+
+    # la x1, testdata4
+    # lb x31, -2(x1)
+
+    # la x1, testdata4
+    # lb x31, -1(x1)
+
+    # la x1, testdata4
+    # lb x31, 0(x1)
+
+    # # test avec base négative
+    # la x1, testdata
+    # addi x1, x1, -32
+    # lb x31, 32(x1)
+
+    # # test avec base non alignée
+    # la x1, testdata
+    # addi x1, x1, -6
+    # lb x31, 7(x1)
+
+    # # test écriture après lw
+    # la x1, testdata
+    # lb x31, 0(x1)
+    # li x31, 2
+
+    # la x1, testdata
+    # lb x31, 0(x1)
+    # nop
+    # li x31, 2
+
+
+	# max_cycle 300
+	# pout_start
+	# pout_end
+	
+	# ffffffff
+    # 00000000
+    # fffffff0
+    # 0000000f
+    # ffffffff
+    # 00000000
+    # fffffff0
+    # 0000000f
+    # ffffffff
+    # 00000000
+    # ffffffff
+    # 00000002
+    # ffffffff
+    # 00000002
+
+	testdata:
+	testdata1:  .byte 0xff
+	testdata2:  .byte 0x00
+	testdata3:  .byte 0xf0
+	testdata4:  .byte 0x0f
