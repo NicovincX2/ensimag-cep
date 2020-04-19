c62dd6ca183726cdd68af2225a4f54b60528a170
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2020-04-19 18:54:25 +0200

    Update sb.s

--- program/autotest/sb.s
+++ program/autotest/sb.s
@@ -6,43 +6,43 @@
 	# pas à cause d'un problème de génération du fichier .mem
 	# Offset positif
 	# la x1, testdata
-	# li x2, 0x00aa00aa
+	# li x2, 0xffffffaa
 	# sb x2, 0(x1)
 	# lb x31, 0(x1)
 
     # la x1, testdata
-	# li x2, 0xaa00aa00
-	# sb x2, 4(x1)
-	# lb x31, 4(x1)
+	# li x2, 0xffffffaa
+	# sb x2, 1(x1)
+	# lb x31, 1(x1)
 	
 	# la x1, testdata
-	# li x2, 0x0aa00aa0
-	# sb x2, 8(x1)
-	# lb x31, 8(x1)
+	# li x2, 0xffffffa0
+	# sb x2, 2(x1)
+	# lb x31, 2(x1)
 	
 	# la x1, testdata
-	# li x2, 0xa00aa00a
-	# sb x2, 12(x1)
-	# lb x31, 12(x1)
+	# li x2, 0x0000000a
+	# sb x2, 3(x1)
+	# lb x31, 3(x1)
 	
 	# Offset négatif
 	# la x1, testdata8
-	# li x2, 0x00aa00aa
-	# sb x2, -12(x1)
-	# lb x31, -12(x1)
+	# li x2, 0xffffffaa
+	# sb x2, -3(x1)
+	# lb x31, -3(x1)
 
     # la x1, testdata8
-	# li x2, 0xaa00aa00
-	# sb x2, -8(x1)
-	# lb x31, -8(x1)
+	# li x2, 0x00000000
+	# sb x2, -2(x1)
+	# lb x31, -2(x1)
 	
 	# la x1, testdata8
-	# li x2, 0x0aa00aa0
-	# sb x2, -4(x1)
-	# lb x31, -4(x1)
+	# li x2, 0xffffffa0
+	# sb x2, -1(x1)
+	# lb x31, -1(x1)
 	
 	# la x1, testdata8
-	# li x2, 0xa00aa00a
+	# li x2, 0x0000000a
 	# sb x2, 0(x1)
 	# lb x31, 0(x1)
 	
@@ -55,8 +55,8 @@
 	
 	# Base non alignée
 	# la  x1, testdata9
-    # li  x2, 0x58213098
-    # addi x1, x1, -3
+    # li  x2, 0x00003098
+    # addi x1, x1, -6
     # sb x2, 7(x1)
     # la  x4, testdata10
     # lb x31, 0(x4)
