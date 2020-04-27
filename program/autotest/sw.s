31cde23906e29f2b0fce3253f8a13e3a538532a1
Adrien <adrien.argento2@grenoble-inp.org>
2020-04-27 21:01:11 +0200

    Petit test

--- program/autotest/sw.s
+++ program/autotest/sw.s
@@ -7,19 +7,9 @@
 	sw x1, 4(x2)
 	lw x31, 4(x2)
 
-	lui x1, 54594
-	sw x1, 8(x2)
-	lw x31, 8(x2)
-
-	lui x1, 132314
-	sw x1, 12(x2)
-	lw x31, 12(x2)
-	
 	# max_cycle 300
 	# pout_start
 	# 00000000
-	# 0000D542
-	# 000204DA
 	# pout_end
     
 	.data
