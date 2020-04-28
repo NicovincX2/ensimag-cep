34c0e798c7a1597d4c051be048ef24f1a831574b
n0way <n0way@localhost.localdomain>
2020-04-28 09:26:18 +0200

    lh et lhu tests

--- program/autotest/lhu.s
+++ program/autotest/lhu.s
@@ -4,70 +4,69 @@
 
 	# Les tests qui suivent devraient fonctionner
 	# test avec offset positif
-    # la x1, testdata
-    # lhu x31, 0(x1)
+    la x1, testdata
+    lhu x31, 0(x1)
 
-    # la x1, testdata
-    # lhu x31, 2(x1)
+    la x1, testdata
+    lhu x31, 2(x1)
 
-    # la x1, testdata
-    # lhu x31, 4(x1)
+    la x1, testdata
+    lhu x31, 4(x1)
 
-    # la x1, testdata
-    # lhu x31, 6(x1)
+    la x1, testdata
+    lhu x31, 6(x1)
 
     # test avec offset négatif
-    # la x1, testdata4
-    # lhu x31, -6(x1)
+    la x1, testdata4
+    lhu x31, -6(x1)
 
-    # la x1, testdata4
-    # lhu x31, -4(x1)
+    la x1, testdata4
+    lhu x31, -4(x1)
 
-    # la x1, testdata4
-    # lhu x31, -2(x1)
+    la x1, testdata4
+    lhu x31, -2(x1)
 
-    # la x1, testdata4
-    # lhu x31, 0(x1)
+    la x1, testdata4
+    lhu x31, 0(x1)
 
-    # # test avec base négative
-    # la x1, testdata
-    # addi x1, x1, -32
-    # lhu x31, 32(x1)
+    # test avec base négative
+    la x1, testdata
+    addi x1, x1, -32
+    lhu x31, 32(x1)
 
-    # # test avec base non alignée
-    # la x1, testdata
-    # addi x1, x1, -5
-    # lhu x31, 7(x1)
+    # test avec base non alignée
+    la x1, testdata
+    addi x1, x1, -5
+    lhu x31, 7(x1)
 
-    # # test écriture après lw
-    # la x1, testdata
-    # lhu x31, 0(x1)
-    # li x31, 2
+    # test écriture après lw
+    la x1, testdata
+    lhu x31, 0(x1)
+    li x31, 2
 
-    # la x1, testdata
-    # lhu x31, 0(x1)
-    # nop
-    # li x31, 2
+    la x1, testdata
+    lhu x31, 0(x1)
+    nop
+    li x31, 2
 
 
 	# max_cycle 300
 	# pout_start
+    # 000000ff
+    # 0000ff00
+    # 00000ff0
+    # 0000f00f
+    # 000000ff
+    # 0000ff00
+    # 00000ff0
+    # 0000f00f
+    # 000000ff
+    # 0000ff00
+    # 000000ff
+    # 00000002
+    # 000000ff
+    # 00000002
 	# pout_end
-	
-	# 000000ff
-    # 0000ff00
-    # 00000ff0
-    # 0000f00f
-    # 000000ff
-    # 0000ff00
-    # 00000ff0
-    # 0000f00f
-    # 000000ff
-    # 0000ff00
-    # 000000ff
-    # 00000002
-    # 000000ff
-    # 00000002
 
     .data
 	testdata:
