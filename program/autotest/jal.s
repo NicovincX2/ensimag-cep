d1bf8388a4ceadd2ff5c19f39d8f886a5e01c344
n0way <n0way@localhost.localdomain>
2020-04-28 11:47:55 +0200

    tests okay

--- program/autotest/jal.s
+++ program/autotest/jal.s
@@ -2,11 +2,6 @@
 
     .text
 
-    #li  ra, 0
-    #li x31, 42  # on a 42 si tout se passe bien
-
-    #jal x4, target_2
-
 	lui x31, 0
 	jal x0, etiquette_test
 	j fin
@@ -20,34 +15,40 @@
 	fin:
 		lui x31, 0
 
-    #linkaddr_2:
+    # li  ra, 0
+    # li x31, 42  # on a 42 si tout se passe bien
+
+    # jal x4, target_2
+
+    # linkaddr_2:
     #    nop
     #    nop
     #    j fail
 
-    #target_2:
+    # target_2:
     #    la  x2, linkaddr_2
     #    bne x2, x4, fail
 
-    #fail:
+    # fail:
     #    li x31, -42
 
     # un original
-    #li ra, 1
-    #jal x0, 1f
-    #addi ra, ra, 1
-    #addi ra, ra, 1
-    #addi ra, ra, 1
-    #addi x31, ra, 1
-    #1:
-    #    addi ra, ra, 1
-    #    addi x31, ra, 1
+    li ra, 1
+    jal x0, 1f
+    addi ra, ra, 1
+    addi ra, ra, 1
+    addi ra, ra, 1
+    addi x31, ra, 1
+    1:
+       addi ra, ra, 1
+       addi x31, ra, 1
 
     # max_cycle 300
 	# pout_start
     # 00000000
 	# FFFFF000
 	# 00000000
+    # 00000003
     # pout_end
 
     # 0000002a
