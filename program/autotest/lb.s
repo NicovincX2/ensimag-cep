7e81643b7151556e441d138db9c0b257c52a5edb
n0way <n0way@localhost.localdomain>
2020-04-28 10:21:45 +0200

    1 test

--- program/autotest/lb.s
+++ program/autotest/lb.s
@@ -7,39 +7,39 @@
     la x1, testdata
     lb x31, 0(x1)
 
-    la x1, testdata
-    lb x31, 1(x1)
+    # la x1, testdata
+    # lb x31, 1(x1)
 
-    la x1, testdata
-    lb x31, 2(x1)
+    # la x1, testdata
+    # lb x31, 2(x1)
 
-    la x1, testdata
-    lb x31, 3(x1)
+    # la x1, testdata
+    # lb x31, 3(x1)
 
-    # test avec offset négatif
-    la x1, testdata4
-    lb x31, -3(x1)
+    # # test avec offset négatif
+    # la x1, testdata4
+    # lb x31, -3(x1)
 
-    la x1, testdata4
-    lb x31, -2(x1)
+    # la x1, testdata4
+    # lb x31, -2(x1)
 
-    la x1, testdata4
-    lb x31, -1(x1)
+    # la x1, testdata4
+    # lb x31, -1(x1)
 
-    la x1, testdata4
-    lb x31, 0(x1)
+    # la x1, testdata4
+    # lb x31, 0(x1)
 
-    # test avec base négative
-    la x1, testdata
-    addi x1, x1, -32
-    lb x31, 32(x1)
+    # # test avec base négative
+    # la x1, testdata
+    # addi x1, x1, -32
+    # lb x31, 32(x1)
 
-    # test avec base non alignée
-    la x1, testdata
-    addi x1, x1, -6
-    lb x31, 7(x1)
+    # # test avec base non alignée
+    # la x1, testdata
+    # addi x1, x1, -6
+    # lb x31, 7(x1)
 
-    # test écriture après lw
+    # # test écriture après lw
     # la x1, testdata
     # lb x31, 0(x1)
     # li x31, 2
@@ -53,17 +53,17 @@
 	# max_cycle 300
 	# pout_start
     # ffffffff
-    # 00000000
-    # fffffff0
-    # 0000000f
-    # ffffffff
-    # 00000000
-    # fffffff0
-    # 0000000f
-    # ffffffff
-    # 00000000
 	# pout_end
 
+    # 00000000
+    # fffffff0
+    # 0000000f
+    # ffffffff
+    # 00000000
+    # fffffff0
+    # 0000000f
+    # ffffffff
+    # 00000000
     # ffffffff
     # 00000002
     # ffffffff
