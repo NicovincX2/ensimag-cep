817594aa2e2e9d6c6f3866f62f9ae8aacdb6db10
n0way <n0way@localhost.localdomain>
2020-04-29 10:45:00 +0200

    lh base neg

--- program/autotest/lh.s
+++ program/autotest/lh.s
@@ -13,37 +13,37 @@
     # lh x31, 6(x1)
 
     # test avec offset négatif
-    la x1, testdata2
-    lh x31, -6(x1)
+    # la x1, testdata2
+    # lh x31, -6(x1)
 
-    la x1, testdata2
-    lh x31, -4(x1)
+    # la x1, testdata2
+    # lh x31, -4(x1)
 
+    # la x1, testdata1
+    # lh x31, -2(x1)
+
+    # la x1, testdata1
+    # lh x31, 0(x1)
+
+    # test avec base négative
     la x1, testdata1
-    lh x31, -2(x1)
+    addi x1, x1, -32
+    lh x31, 32(x1)
+
+    # test avec base non alignée
+    la x1, testdata1
+    addi x1, x1, -5
+    lh x31, 7(x1)
+
+    # test écriture après lh
+    la x1, testdata1
+    lh x31, 0(x1)
+    li x31, 2
 
     la x1, testdata1
     lh x31, 0(x1)
-
-    # test avec base négative
-    #la x1, testdata
-    #addi x1, x1, -32
-    #lh x31, 32(x1)
-
-    # test avec base non alignée
-    #la x1, testdata
-    #addi x1, x1, -5
-    #lh x31, 7(x1)
-
-    # test écriture après lh
-    #la x1, testdata
-    #lh x31, 0(x1)
-    #li x31, 2
-
-    #la x1, testdata
-    #lh x31, 0(x1)
-    #nop
-    #li x31, 2
+    nop
+    li x31, 2
 
 
 	# max_cycle 300
@@ -52,20 +52,18 @@
     # ffffff00
     # 000000ff
     # ffffff00
-    # 00000ff0
-    # fffff00f
+    # 000000ff
+    # 00000002
+    # 000000ff
+    # 00000002
     # pout_end
 
     # 00000ff0
     # fffff00f
-
-    
     # 000000ff
     # ffffff00
-    # 000000ff
-    # 00000002
-    # 000000ff
-    # 00000002
+    # 00000ff0
+    # fffff00f
 
     .data
 	testdata1: .half 0x00ff, 0xff00
